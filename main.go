e2fcc54d3b5907c07381d3d5451530982e710998
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-11-17 01:19:31 +0530

    Handle reconnections and disconnections

--- main.go
+++ main.go
@@ -37,61 +37,71 @@ func main() {
 		handler(args)
 	})
 
-	// Listening for new connections (this is a blocking connection) and whenever
-	// a connection is made then an acceptance is established using Accept()
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
-
-	// Writer allocation for writing back to redis-cli
-	writer := NewWriter(conn)
-
-	// Create an infinite for-loop so that we can keep listening to the port
-	// constantly, receive commands from clients and respond to them
+	// When a connection drops, we continue listening for a new connection
 	for {
-		resp := NewResp(conn)
-		value, err := resp.Read()
+		// Listening for new connections (this is a blocking connection) and whenever
+		// a connection is made then an acceptance is established using Accept()
+		conn, err := l.Accept()
 		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Connection dropped to Bluedis server.")
-				return
-			}
 			fmt.Println(err)
 			return
 		}
+		defer conn.Close()
 
-		if value.typ != "array" {
-			fmt.Println("Invalid request, expected array")
-			continue
-		}
+		// Writer allocation for writing back to redis-cli
+		writer := NewWriter(conn)
 
-		if len(value.array) == 0 {
-			fmt.Println("Invalid request, expected array length > 0")
-			continue
-		}
+		// Create an infinite for-loop so that we can keep listening to the port
+		// constantly, receive commands from clients and respond to them
+		for {
+			resp := NewResp(conn)
+			value, err := resp.Read()
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("Client disconnected from Bluedis server.")
+					break
+				}
+				fmt.Println(err)
+				break
+			}
 
-		command := strings.ToUpper(value.array[0].bulk)
-		args := value.array[1:]
+			if value.typ != "array" {
+				fmt.Println("Invalid request, expected array")
+				continue
+			}
 
-		handler, ok := Handlers[command]
-		if !ok {
-			fmt.Println("Invalid command: ", command)
-			writer.Write(Value{typ: "string", str: ""})
-			continue
-		}
+			if len(value.array) == 0 {
+				fmt.Println("Invalid request, expected array length > 0")
+				continue
+			}
 
-		// Append "write" commands to AOF
-		if command == "SET" || command == "HSET" {
-			aof.Write(value)
-		}
+			command := strings.ToUpper(value.array[0].bulk)
+			args := value.array[1:]
 
-		result := handler(args)
-		err = writer.Write(result)
-		if err != nil {
-			fmt.Println(err)
+			handler, ok := Handlers[command]
+			// Redis sends an initial command when connecting, handling it
+			if command == "COMMAND" || command == "RETRY" {
+				fmt.Println("Client connected to Bluedis server!")
+				writer.Write(Value{typ: "string", str: ""})
+				continue
+			}
+			if !ok {
+				fmt.Println("Invalid command: ", command)
+				writer.Write(Value{typ: "string", str: ""})
+				continue
+			}
+
+			// Append "write" commands to AOF
+			if command == "SET" || command == "HSET" {
+				aof.Write(value)
+			}
+
+			result := handler(args)
+			err = writer.Write(result)
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
+
 }
